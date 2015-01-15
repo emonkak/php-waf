aaf4d37c6c501a65c78d2856bb0c8c194f521f9a
Shota Nozaki <emonkak@gmail.com>
2015-01-16 01:09:03 +0900

    Implement ProfilerMiddleware

---
+++ src/Middleware/ProfilerMiddleware.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Emonkak\Framework\Middleware;
+
+use Emonkak\Framework\Exception\HttpException;
+use Emonkak\Framework\KernelInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Profiler\Profiler;
+
+class ProfilerMiddleware implements KernelInterface
+{
+    /**
+     * @var KernelInterface
+     */
+    private $kernel;
+
+    /**
+     * @var Profiler
+     */
+    private $profiler;
+
+    /**
+     * @param KernelInterface $kernel
+     * @param Profiler        $profiler
+     */
+    public function __construct(KernelInterface $kernel, Profiler $profiler)
+    {
+        $this->kernel = $kernel;
+        $this->profiler = $profiler;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function handleRequest(Request $request)
+    {
+        $response = $this->kernel->handleRequest($request);
+        $profile = $this->profiler->collect($request, $response);
+
+        $this->profiler->saveProfile($profile);
+
+        return $response;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function handleException(Request $request, HttpException $exception)
+    {
+        $response = $this->kernel->handleException($request, $exception);
+        $profile = $this->profiler->collect($request, $response, $exception);
+
+        $this->profiler->saveProfile($profile);
+
+        return $response;
+    }
+}
---
+++ tests/Middleware/ProfilerMiddlewareTest.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Emonkak\Framework\Tests\Middleware;
+
+use Emonkak\Framework\Exception\HttpException;
+use Emonkak\Framework\Middleware\ProfilerMiddleware;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Profiler\Profile;
+
+class ProfilerMiddlewareMiddlewareTest extends \PHPUnit_Framework_TestCase 
+{
+    public function testHandleRequest()
+    {
+        $request = new Request();
+        $response = new Response();
+        $profile = new Profile('token');
+
+        $kernel = $this->getMock('Emonkak\Framework\KernelInterface');
+        $kernel
+            ->expects($this->once())
+            ->method('handleRequest')
+            ->with($this->identicalTo($request))
+            ->willReturn($response);
+
+        $profiler = $this->getMockBuilder('Symfony\Component\HttpKernel\Profiler\Profiler')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $profiler
+            ->expects($this->once())
+            ->method('collect')
+            ->with($this->identicalTo($request), $this->identicalTo($response))
+            ->willReturn($profile);
+        $profiler
+            ->expects($this->once())
+            ->method('saveProfile')
+            ->with($this->identicalTo($profile));
+
+        $middleware = new ProfilerMiddleware($kernel, $profiler);
+        $this->assertSame($response, $middleware->handleRequest($request));
+    }
+
+    public function testHandleException()
+    {
+        $request = new Request();
+        $response = new Response();
+        $profile = new Profile('token');
+        $exception = new HttpException(404);
+
+        $kernel = $this->getMock('Emonkak\Framework\KernelInterface');
+        $kernel
+            ->expects($this->once())
+            ->method('handleException')
+            ->with($this->identicalTo($request), $this->identicalTo($exception))
+            ->willReturn($response);
+
+        $profiler = $this->getMockBuilder('Symfony\Component\HttpKernel\Profiler\Profiler')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $profiler
+            ->expects($this->once())
+            ->method('collect')
+            ->with($this->identicalTo($request), $this->identicalTo($response), $this->identicalTo($exception))
+            ->willReturn($profile);
+        $profiler
+            ->expects($this->once())
+            ->method('saveProfile')
+            ->with($this->identicalTo($profile));
+
+        $middleware = new ProfilerMiddleware($kernel, $profiler);
+        $this->assertSame($response, $middleware->handleException($request, $exception));
+    }
+}
